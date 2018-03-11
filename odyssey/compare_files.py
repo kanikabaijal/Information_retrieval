2d1e63d0612634349dd50185965504ddeeea19fe
Kanika Baijal <kanikabaijal554@gmail.com>
2018-03-11 11:22:05 -0700

    initial project version

---
+++ odyssey/compare_files.py
@@ -0,0 +1,36 @@
+import argparse
+from collections import defaultdict
+from tokenizer import tokenize
+from tokenizer import compute_word_frequencies
+from tokenizer import print_sorted_order
+
+def count_common(frequency1, frequency2, print_common_keys=False):
+    counter = 0
+    for k, v in frequency1.items():
+        if k in frequency2:
+            counter+=1
+            if print_common_keys: print(k, end=',')
+    if print_common_keys: print()
+    return counter
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filepath1", help="path of the file to compare")
+    parser.add_argument("filepath2", help="path of the file to compare")
+    parser.add_argument("print_common", help="True/T to display common keys")
+    args = parser.parse_args()
+    filepath_1 = args.filepath1
+    filepath_2 = args.filepath2
+    print_common_flag = args.print_common
+    tokens_1 = tokenize(filepath_1)
+    tokens_2 = tokenize(filepath_2)
+    word_frequencies_1 = compute_word_frequencies(tokens_1)
+    word_frequencies_2 = compute_word_frequencies(tokens_2)
+    if print_common_flag in ['True', 'true', 'T', 't']:
+        count = count_common(word_frequencies_1, word_frequencies_2, True)
+    else:
+        count = count_common(word_frequencies_1, word_frequencies_2)
+    print(count)
+
+if __name__=='__main__':
+    main()
---
+++ odyssey/tokenizer.py
@@ -0,0 +1,51 @@
+import argparse
+import re
+import codecs
+from collections import defaultdict
+from collections import Counter
+
+separator = ' '
+
+def print_sorted_order(frequency, **kwargs):
+    freq_sort = sorted(frequency.items(), key=lambda x:x[1], reverse=True)
+
+    if 'output_file_name' in kwargs:
+        with codecs.open(kwargs['output_file_name'], 'w+', 'utf-8') as f:
+            for (k,v) in freq_sort:
+                f.write(k+','+str(v)+'\n')
+    else:
+        for (k,v) in freq_sort:
+            print(k, v)
+
+
+def compute_word_frequencies(tokens):
+    frequencies = Counter(tokens)
+    return frequencies
+
+
+def tokenize(text_file_path):
+    tokens = []
+    with open(text_file_path, encoding="utf-8") as file:
+        for line in file:
+            for word in line.split(separator):
+                word = re.sub('[^A-Za-z\d]+', '', word)
+                if len(word)!=0: tokens.append(str.lower(word))
+    return tokens
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filepath", help="path of the file to tokenize")
+    parser.add_argument("--output_file", help="path of the output file")
+    args = parser.parse_args()
+    filepath = args.filepath
+    tokens = tokenize(filepath)
+    word_frequencies = compute_word_frequencies(tokens)
+    if args.output_file:
+        print_sorted_order(word_frequencies, output_file_name=args.output_file)
+    else:
+        print_sorted_order(word_frequencies)
+        
+
+if __name__=='__main__':
+    main()
